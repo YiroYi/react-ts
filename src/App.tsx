f0481816a3ec2e51c5621877c83665c84490e368
YiroYi <yiroyi@live.com>
2021-11-21 23:19:13 -0600

    Adding Delete function

--- src/App.tsx
+++ src/App.tsx
@@ -8,14 +8,28 @@ const App: React.FC = () => {
   const [todos, setTodos] = useState<Todo[]>([]);
 
   const todoAddHandler = (text: string) => {
-    setTodos([{id: Math.random().toString(), text: text}]);
+    setTodos(prevTodos => [
+      ...prevTodos,
+      {id: Math.random().toString(), text: text}
+    ]);
+  }
+
+  const todoDeleteHandler = (todoId: string) => {
+    setTodos(prevTodos => {
+      return prevTodos.filter(todo => todo.id !== todoId);
+    });
   }
 
   return (
     <div className="App">
       {/* A component that adds todos */}
-      <NewTodo onAddTodo = {todoAddHandler} />
-      <TodoList items={todos} />
+      <NewTodo
+        onAddTodo = {todoAddHandler}
+      />
+      <TodoList
+        items={todos}
+        onDeleteTodo={todoDeleteHandler}
+      />
     </div>
   );
 };
